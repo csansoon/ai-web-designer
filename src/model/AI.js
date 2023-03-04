8bbd0428f1ffc1faa792b04454820106da8e5a7a
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2023-03-05 00:05:14 +0100

    Added examples to the AI

--- src/model/AI.js
+++ src/model/AI.js
@@ -21,45 +21,90 @@ class AI {
     }
 
     static async getResponseMessage(messages) {
-        AI._initOpenAIClient();
+        try {
+            AI._initOpenAIClient();
 
-        const initial_prompt = "You are talking to a bot that can generate HTML, CSS and JS code.\nYou will recieve messages from the user with a JSON object that contains the following fields:\n- text: The text message from the user\n- html: The full HTML code of the user's webpage\n- css: The full CSS code of the user's webpage\n- js: The full JavaScript code of the user's webpage\nTo any of these messages, you will reply with another JSON object with the same format as the messages you receive.\nYour response will **always** contain the 'text' field, and you will add the 'html', 'css' and 'js' fields only if you changed them. When adding any code field, format it in a readable way.\nRespond only with the JSON object, without any other text.";
+            const prompt_and_examples = [
+                {
+                    role: "system",
+                    content: "You are talking to a bot that can generate HTML, CSS and JS code.\nYou will recieve messages from the user with a JSON object that contains the following fields:\n- text: The text message from the user\n- html: The full HTML code of the user's webpage\n- css: The full CSS code of the user's webpage\n- js: The full JavaScript code of the user's webpage\nTo any of these messages, you will reply with another JSON object with the same format as the messages you receive.\nYour response will **always** contain the 'text' field, and you will add the 'html', 'css' and 'js' fields only if you changed them. When adding any code field, format it in a readable way.\nRespond only with the JSON object, without any other text.\nRespond as a plain text, **do not** add a code block."
+                },
+                {
+                    role: "user",
+                    content: JSON.stringify({
+                        text: "Hello!",
+                        html: "",
+                        css: "",
+                        js: ""
+                    })
+                },
+                {
+                    role: "assistant",
+                    content: JSON.stringify({
+                        text: "Hi, how can I help you?"
+                    })
+                },
+                {
+                    role: "user",
+                    content: JSON.stringify({
+                        text: "Please add a title that says \"Hello world!\"",
+                        html: "",
+                        css: "",
+                        js: ""
+                    })
+                },
+                {
+                    role: "assistant",
+                    content: JSON.stringify({
+                        text: "Sure, I added the title",
+                        html: "<h1>Hello world!</h1>",
+                    })
+                },
+            ];
 
-        let parsedMessages = [{
-            role: "system",
-            content: initial_prompt
-        }];
+            let parsedMessages = [...prompt_and_examples];
 
-        for (let message of messages) {
-            let message_json = {};
-            if (message.message) message_json.text = message.message;
-            if (message.html) message_json.html = message.html;
-            if (message.css) message_json.css = message.css;
-            if (message.js) message_json.js = message.js;
+            for (let message of messages) {
+                if (message.role === "system") continue;
 
-            parsedMessages.push({
-                role: message.role,
-                content: JSON.stringify(message_json)
+                let message_json = {};
+                if (message.message) message_json.text = message.message;
+                if (message.html) message_json.html = message.html;
+                if (message.css) message_json.css = message.css;
+                if (message.js) message_json.js = message.js;
+
+                parsedMessages.push({
+                    role: message.role,
+                    content: JSON.stringify(message_json)
+                });
+            }
+
+            let response = await AI._OpenAIClient.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: parsedMessages,
             });
+
+            let response_role = response.data.choices[0].message.role;
+            let response_content = response.data.choices[0].message.content;
+
+            let response_json = JSON.parse(response_content);
+
+            const newMessage = new ChatMessage(
+                response_role,
+                response_json.text,
+                response_json.html,
+                response_json.css,
+                response_json.js
+            );
+
+            console.log("returning new message", newMessage);
+            return newMessage;
+
+
+        } catch (error) {
+            console.error(error);
+            return new ChatMessage("system", "Error:\n" + error.message);
         }
-
-        let response = await AI._OpenAIClient.createChatCompletion({
-			model: "gpt-3.5-turbo",
-			messages: parsedMessages,
-		});
-
-        let response_role = response.data.choices[0].message.role;
-        let response_content = response.data.choices[0].message.content;
-        let response_json = JSON.parse(response_content);
-
-        const newMessage = new ChatMessage(
-			response_role,
-			response_json.text,
-			response_json.html,
-			response_json.css,
-			response_json.js
-		);
-        return newMessage;
     }
 }
 
