efe8b7669a60880724c20c951f3b65d19ccc7840
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2023-03-04 17:28:02 +0100

    Restructured files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+	<App />
   </React.StrictMode>
 );
 
